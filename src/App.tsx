84a1bd2c524f80502f7e787239aff1d74b95ce45
SeobisBack <seobisback@gmail.com>
2021-11-17 18:58:45 +0900

    Learn React js and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Number from './Number';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  counter:number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter : 0
+  }
+    render () {
+      const { counter } = this.state;
+      return (
+      <div>
+        <Number count={counter}/> <button onClick={this.add}>Add</button>
+      </div>
+      );
+    }
+    add = () => {
+      this.setState(prev => {
+        return{
+          counter : prev.counter + 1
+        }
+      })
+    }
 }
 
 export default App;
